531c78d35be0b36fa3db64828b0c61c7c60b8fcb
vonce <usschuang@gmail.com>
2018-08-31 17:59:12 -0700

    input fields working, added SQLAlchemy

--- flaskRoute.py
+++ flaskRoute.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchecmy import SQLAlchemy
+from forms import RegistrationForm, LoginForm=
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '39a4bbaf79d10771aeed528b03e996ac'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f"User Email('{self.email}')"
 
 posts = [
     {
@@ -16,11 +30,30 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',posts=posts)
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('home.html')
+    return render_template('about.html', title='About')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'{form.email.data} logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.email.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
